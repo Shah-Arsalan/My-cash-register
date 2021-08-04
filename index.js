8e7baf7c35d00f2878a4ded0ab739d2d31b8fa5f
Shah-Arsalan <shaharslan2278@gmail.com>
2021-08-04 07:42:09 +0530

    Added html , css and index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var billamount = document.querySelector("#bill-amount");
+var cashgiven = document.querySelector("#cash-given");
+var changebutton = document.querySelector("#click");
+
+changebutton.addEventListener("click", clickButton)
+
+function clickbutton()
+{
+    if (billamount.value >= 0){
+        if (cashamount.value > billamount.value){
+            function getchage
+        }
+    }else {
+        alert ("sfhsfhiuh")
+    }
+}
