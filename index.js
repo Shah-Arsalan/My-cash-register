8e88fb7d5a35f177b53068fa4e4c0c83c2a0ad22
Shah-Arsalan <shaharslan2278@gmail.com>
2021-08-05 06:40:37 +0530

    Done with the logic part

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-var billamount = document.querySelector("#bill-amount");
-var cashgiven = document.querySelector("#cash-given");
-var changebutton = document.querySelector("#click");
+const billamount = document.querySelector("#bill-amount");
+const cashgiven = document.querySelector("#cash-given");
+const changebutton = document.querySelector("#click");
+const message = document.querySelector("#error-message");
+const notesavailable = [2000,500,100,20,10,5,1];
+const numofnotes = document.querySelector("#number-of-notes");
 
-changebutton.addEventListener("click", clickButton)
 
-function clickbutton()
-{
-    if (billamount.value >= 0){
-        if (cashamount.value > billamount.value){
-            function getchage
+
+changebutton.addEventListener("click",function clickbutton(){
+    message.style.display="none";
+    if(billamount.value > 0){
+        if(cashgiven.value >= billamount.value){
+           const returnamount = cashgiven.value - billamount.value;
+           changecalculator(returnamount);
+
+        }else{
+            messagegiven("Cash given is less than the billamount . Chal paisa nikal");
         }
-    }else {
-        alert ("sfhsfhiuh")
+
+    }else{
+        messagegiven("The amount entered is invalid");
     }
-}
+
+});
+
+function messagegiven (text){
+    message.style.display="block";
+    message.innerText =text;
+    }
+
+    function changecalculator(returnamount){
+        for(i=0;i < notesavailable.length; i++){
+            const numberofnotes = Math.trunc(returnamount / notesavailable[i]  ); 
+            returnamount %= notesavailable[i]; 
+            numofnotes[i].innerText = numberofnotes;
+
+        }
+
+    }
