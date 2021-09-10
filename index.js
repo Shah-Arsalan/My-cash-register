508f7a96143890f0d20ae82c9d0c248b29b65d47
Shah-Arsalan <shaharslan2278@gmail.com>
2021-09-11 00:27:34 +0530

    bonus added

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const changebutton = document.querySelector("#click");
 const message = document.querySelector("#error-message");
 const notesavailable = [2000,500,100,20,10,5,1];
 const numofnotes = document.querySelectorAll(".number-of-notes");
+const label = document.querySelector("#label-id");
+const enterbutton = document.querySelector("#click2")
+
+
+label.style.display = "none";
+cashgiven.style.display = "none";
+changebutton.style.display = "none"
+
+// billamount.addEventListener("input", visible);
+
+// function visible (){
+//     label.style.display = "block";
+// cashgiven.style.display = "block";
+// }
+
+enterbutton.addEventListener("click", visible);
+
+function visible(){
+    label.style.display = "block";
+    cashgiven.style.display = "block";
+    changebutton.style.display = "block"
+    
+}
+
 
 
 
